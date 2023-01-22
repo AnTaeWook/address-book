4917ffe900767c66e7561792aa37a8549ae2e95d
AnTaeWook <antk7894@naver.com>
2023-01-22 16:10:08 +0900

    #25 - 입력 값 검증
    주소록 생성 시 받는 Dto 의 입력 값들의
    검증을 위한 라이브러리를 추가하고,
    
    어노테이션을 통해 검증을 수행하도록 하였음.

--- address/src/main/java/assignment/address/controller/AddressController.java
+++ address/src/main/java/assignment/address/controller/AddressController.java
@@ -3,17 +3,16 @@ package assignment.address.controller;
 import assignment.address.dto.RequestAddressDto;
 import assignment.address.dto.ResponseAddressDto;
 import assignment.address.service.AddressService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
-import java.net.URL;
 
 @RequiredArgsConstructor
 @RequestMapping("/addresses")
@@ -34,14 +33,14 @@ public class AddressController {
     }
 
     @PostMapping
-    public ResponseEntity<ResponseAddressDto> create(@RequestBody RequestAddressDto addressDto) {
+    public ResponseEntity<ResponseAddressDto> create(@RequestBody @Valid RequestAddressDto addressDto) {
         ResponseAddressDto responseAddressDto = addressService.saveAddress(addressDto);
         return ResponseEntity.created(URI.create("/addresses/" + responseAddressDto.getId())).
                 body(responseAddressDto);
     }
 
     @PutMapping("/{addressId}")
-    public ResponseEntity<ResponseAddressDto> edit(@PathVariable Long addressId, RequestAddressDto addressDto) {
+    public ResponseEntity<ResponseAddressDto> edit(@PathVariable Long addressId, @RequestBody RequestAddressDto addressDto) {
         return ResponseEntity.ok(addressService.updateAddress(addressId, addressDto));
     }
 
